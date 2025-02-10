124001edf323e1c26d21f4af43098252c74e1286
IlyaPodkin <ilhapodkin@yandex.ru>
2025-02-10 15:28:45 +0400

    Создание модели данных Message.cs

---
+++ Messenger/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace Messenger.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int Order { get; set; }
+    }
+}
