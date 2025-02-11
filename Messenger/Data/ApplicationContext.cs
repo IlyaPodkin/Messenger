abe59b32438609204fc4200b27267cbcc4812c34
IlyaPodkin <ilhapodkin@yandex.ru>
2025-02-11 17:51:40 +0400

    Добавление docker-compose.yml и контекста для создания таблицы для хранения информации о переписке

---
+++ Messenger/Data/ApplicationContext.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Npgsql;
+
+namespace Messenger.Data
+{
+    public class ApplicationContext
+    {
+        private readonly string _connectionString;
+        public ApplicationContext(string connectionString) 
+        {
+            _connectionString = connectionString;
+        }
+        public async Task CreateTable() 
+        {
+            using(var connection = new NpgsqlConnection(_connectionString)) 
+            {
+                await connection.OpenAsync();
+                var createTableQuery = 
+                    @"CREATE TABLE IF NOT EXISTS Messages(
+                        id SERIAL PRIMARY KEY,
+                        messageContent VARCHAR(128) NOT NULL, 
+                        timeStamp TIMESTAMP DEFAULT TIMESTAMP CURRENT_TIMESTAMP,
+                        order INT NOT NULL
+        `           )";
+                await connection.ExecuteAsync(createTableQuery);
+                Console.WriteLine("Таблица создана");
+            }
+        }
+    }
+}
--- Messenger/Program.cs
+++ Messenger/Program.cs
@@ -2,6 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
 builder.Services.AddControllers();
 
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsetings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var connectionString = configuration.GetConnectionString("DefaultConction");
+
 var app = builder.Build();
 
 app.Run();
